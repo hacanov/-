b2a0ce18ea96031f15703d8476ff8bdd76c751c3
hacanov <123230509+hacanov@users.noreply.github.com>
2023-02-05 13:08:37 +0300

    Add files via upload

---
+++ oop_2.py
@@ -0,0 +1,12 @@
+class math:
+    def __init__(self,a,b):
+         self.a, self.b = a,b
+    def addition(self):
+         print( self.a+self.b)
+    def multiplication(self):
+         print( self.a*self.b)
+    def division(self):
+         print( self.a/self.b)
+    def subtraction(self):
+         print(self.a - self.b)
+
